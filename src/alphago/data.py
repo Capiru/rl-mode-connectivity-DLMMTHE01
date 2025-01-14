3ca41f6d6e266c23576a4b3ee92abadcb14c76f2
Capiru <gabriel.silva.2@deliveryhero.com>
2025-01-14 08:41:01 +0100

    refactor dataset and add tests

--- src/alphago/data.py
+++ src/alphago/data.py
@@ -1,62 +1,95 @@
 ### Dataset
 import torch
-from torch.utils.data import Dataset, DataLoader
-from time import perf_counter
-import pandas as pd
-import numpy as np
+from torch.utils.data import Dataset
 from alphago.config import cfg
 
 import logging
+
 logger = logging.getLogger(__name__)
 logger.setLevel("INFO")
 
+
 class GameHistoryDataset(Dataset):
-    def __init__(self, cfg = cfg,min_elo = 0,model_id:int = 0,smoke_test = False, drop_zero_rewards = True):
+    def __init__(
+        self,
+        cfg=cfg,
+        min_elo=0,
+        model_id: int = 0,
+        smoke_test=False,
+        drop_zero_rewards=True,
+        shuffle=True,
+    ):
         self.min_elo = min_elo
         self.smoke_test = smoke_test
-        self.folder_path = f"{cfg.episode_save_path}/{cfg.env_type}/num_sims_{cfg.num_simulations}/"
+        self.folder_path = (
+            f"{cfg.episode_save_path}/{cfg.env_type}/num_sims_{cfg.num_simulations}/"
+        )
         self.df = cfg.episodes_df.copy()
-        
-        if drop_zero_rewards:
-            self.df = self.df.loc[self.df["reward"]!=0]
 
-        self.df = self.df.loc[self.df["elo"]>=min_elo]
+        if drop_zero_rewards:
+            self.df = self.df.loc[self.df["reward"] != 0]
+
+        self.df = self.df.loc[self.df["elo"] >= min_elo]
         if model_id:
-            self.df = self.df.loc[self.df["model_id"]==model_id]
+            self.df = self.df.loc[self.df["model_id"] == model_id]
         df_sorted = self.df.sort_index()
         if cfg.episodes_replay_buffer_size:
             self.df = df_sorted.tail(cfg.episodes_replay_buffer_size)
         self.game_history_list = self.df["game"]
-        
+
         self.df.reset_index(drop=True, inplace=True)
         self.df["episode"] = self.df.index
-        
+
         if "go" in cfg.env_type:
-            self.df["move_count"] = self.df["move_count"]-2
-        
-        self.episode_replay_df = self.df.loc[self.df.index.repeat(self.df["move_count"])].copy()
-        self.episode_replay_df["timestep"] = self.episode_replay_df.groupby(level=0).cumcount()
-        self.episode_replay_df.reset_index(drop=True, inplace=True)  
-        
+            self.df["move_count"] = self.df["move_count"] - 2
+
+        self.episode_replay_df = self.df.loc[
+            self.df.index.repeat(self.df["move_count"])
+        ].copy()
+        self.episode_replay_df["timestep"] = self.episode_replay_df.groupby(
+            level=0
+        ).cumcount()
+        self.episode_replay_df.reset_index(drop=True, inplace=True)
+        if shuffle:
+            self.episode_replay_df = self.episode_replay_df.sample(frac=1).reset_index(
+                drop=True
+            )
 
     def __len__(self):
         if self.smoke_test:
             return 100
         return len(self.episode_replay_df)
-    
+
+    def _get_discounted_reward(self, reward, timestep, episode_len):
+        return reward * cfg.discount_factor ** (episode_len - timestep - 1)
+
+    def _did_player_0_win(self, timestep):
+        return 1 if timestep % 2 == 0 else -1
+
+    def _get_return_reward(self, reward, timestep, episode_len):
+        return self._get_discounted_reward(
+            reward, timestep, episode_len
+        ) * self._did_player_0_win(timestep)
+
     def __getitem__(self, idx):
         # Load tensors from the specified files
         row = self.episode_replay_df.iloc[idx]
-        
+
         try:
-            obs,action = torch.load(str(self.game_history_list.iloc[row["episode"]]))  # Observation Tensor (9,9,17)
-        except Exception as e:
-            print(row,self.game_history_list.iloc[row["episode"]])
-        reward = row["reward"] # Value (1,)
-        episode_len = obs.shape[-1]
-        time_step = int(row["timestep"])
+            obs, action = torch.load(
+                str(self.game_history_list.iloc[row["episode"]])
+            )  # Observation Tensor (9,9,17)
+        except Exception:
+            print(row, self.game_history_list.iloc[row["episode"]])
+        reward = row["reward"]  # Value (1,)
+        episode_len = int(row["move_count"])
+        timestep = int(row["timestep"])
 
-        reward *= cfg.discount_factor**(episode_len-time_step)
-        did_player_win = 1 if time_step % 2 == 0 else -1
-
-        return obs[:,:,:,time_step], action[:,time_step], torch.tensor(reward*did_player_win,dtype=torch.float32)
+        return (
+            obs[:, :, :, timestep],
+            action[:, timestep],
+            torch.tensor(
+                self._get_return_reward(reward, timestep, episode_len),
+                dtype=torch.float32,
+            ),
+        )
---
+++ tests/test_dataset.py
@@ -0,0 +1,44 @@
+from alphago.data import GameHistoryDataset
+import pandas as pd
+
+
+def test_dataset(cfg):
+    cfg.episodes_df = pd.read_csv("tests/data/games.csv")
+    dataset = GameHistoryDataset(cfg, shuffle=False)
+    ## Game 0 - Player 0 wins
+    assert len(dataset) == 12
+    _, _, reward = dataset.__getitem__(0)
+
+    assert round(reward.detach().item(), 4) == 0.9224
+    assert dataset._did_player_0_win(0) == 1
+
+    _, _, reward_4 = dataset.__getitem__(4)
+    assert round(reward_4.detach().item(), 4) == 1.0000
+    assert dataset._did_player_0_win(4) == 1
+
+    _, _, reward_3 = dataset.__getitem__(3)
+
+    assert round(reward_3.detach().item(), 4) == -1.0000 * 0.98
+    assert dataset._did_player_0_win(3) == -1
+
+    ## Game 1 - Player 1 Wins
+    _, _, reward_player_0 = dataset.__getitem__(5)
+    assert reward_player_0.detach().item() < 0.0
+    assert dataset._did_player_0_win(0) == 1
+
+    _, _, reward_player_0_end = dataset.__getitem__(11)
+    assert dataset._did_player_0_win(6) == 1
+    assert round(reward_player_0_end.detach().item(), 4) == -1.0000
+
+    _, _, reward_player_1_end = dataset.__getitem__(10)
+    assert dataset._did_player_0_win(5) == -1
+    assert round(reward_player_1_end.detach().item(), 4) == 0.9800
+
+
+def test_shuffle(cfg):
+    cfg.episodes_df = pd.read_csv("tests/data/games.csv")
+    shuffled_dataset = GameHistoryDataset(cfg, shuffle=True)
+    dataset = GameHistoryDataset(cfg, shuffle=False)
+    assert not shuffled_dataset.episode_replay_df["episode"].equals(
+        dataset.episode_replay_df["episode"]
+    )
