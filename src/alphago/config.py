9cbb5be5c794a65d69fb18d08791af2190969e14
Capiru <gabriel.silva.2@deliveryhero.com>
2025-01-25 16:07:13 +0100

    config v 0.2.0

--- src/alphago/config.py
+++ src/alphago/config.py
@@ -6,23 +6,30 @@ import torch
 
 class Settings(BaseSettings):
     max_episode_length: int = 170
-    max_n_episodes: int = 20000
-    eval_games: int = 10
-    episodes_per_epoch: int = 32
+    max_n_episodes: int = 500000
+    eval_games: int = 400
+    episodes_per_epoch: int = 5000
     elo_bins: int = 50
     eval_every_n_epochs: int = 2
+    random_moves_at_start: int = 8
 
-    episodes_replay_buffer_size: int = 1024
-    batch_samples_from_buffer: int = 8
-    sampling_ratio: float = 0.7
+    episodes_replay_buffer_size: int = 50000
+    sampling_ratio: float = 1.0
 
-    episode_save_path: str = "./data/"
+    episode_save_path: str = "./data_6/"
 
     batch_size: int = 1024
     learning_rate: float = 1e-2
     min_learning_rate: float = 1e-3
+    weight_decay: float = 1e-4
+    dropout: float = 0.2
 
-    env_type: str = "connect4"
+    # Schedulers
+    lr_schedule_max_epochs: int = 500000
+    eps_schedule_max_epochs: int = 50000
+    buffer_size_schedule_max_epochs: int = 15000
+
+    env_type: str = "go_5"
 
     draw_reward: float = 0.3
 
@@ -62,26 +69,57 @@ class Settings(BaseSettings):
             "model_type",
             "episodes",
             "num_parameters",
+            "policy_loss",
+            "value_loss",
         ]
     )
     episodes_df: pd.DataFrame = pd.DataFrame(
-        columns=["game", "actions", "elo", "move_count", "model_id", "reward"]
+        columns=[
+            "game",
+            "actions",
+            "elo",
+            "move_count",
+            "model_id",
+            "reward",
+            "iteration",
+            "player_to_learn",
+            "eval",
+        ]
+    )
+
+    eval_df: pd.DataFrame = pd.DataFrame(
+        columns=[
+            "iteration",
+            "elo_random_play",
+            "move_count_random_play",
+            "elo_random_init",
+            "move_count_random_init",
+            "model_id",
+            "elo_opp",
+            "move_count",
+            "elo_diff",
+            "winrate",
+        ]
     )
 
     # RL Hyperparameters
-    eps: float = 0.9
-    discount_factor: float = 0.85  # Connect 4 - Go 0.98
+    eps: float = 0.25
+    discount_factor: float = 0.98  # Connect 4 - Go 5x5 0.93
+
+    # Game Generation
+    use_models_to_generate_games: bool = False
+    use_only_best_model: bool = True
 
     # MCTS Hyperparams
     temperature: float = 1.0
-    num_simulations: int = 150
+    num_simulations: int = 0
     search_time: int = 0
     dirichlet_alpha: float = 0.1
     puct_coefficient: float = 1.0
     turn_based_flip: bool = True
 
     # Resume Training
-    resume_from: str = ""  # "data/models/go_5_convnet_170_elo_147.0.pth"
+    resume_from: str = ""
 
     device: str = (
         "cuda"
@@ -90,6 +128,3 @@ class Settings(BaseSettings):
         if torch.backends.mps.is_available()
         else "cpu"
     )
-
-
-cfg = Settings()
