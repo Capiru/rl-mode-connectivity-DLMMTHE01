723da2e6830463619679755290a02d56de8abb81
Capiru <gabriel.silva.2@deliveryhero.com>
2025-01-25 13:28:15 +0100

    refactor to not implicitly take env type

--- src/alphago/env.py
+++ src/alphago/env.py
@@ -1,22 +1,20 @@
-from pettingzoo.classic import go_v5,chess_v6,tictactoe_v3,connect_four_v3
-from alphago.config import cfg
+from pettingzoo.classic import go_v5, chess_v6, tictactoe_v3, connect_four_v3
 
-def get_env(env_type = None, cfg = cfg):
-    if not env_type:
-        env_type = cfg.env_type
+
+def get_env(env_type):
     if env_type == "go_5":
-        env = go_v5.raw_env(board_size = 5,komi = 0.5,render_mode='rgb_array')
-        env.reset(seed = 0)
+        env = go_v5.raw_env(board_size=5, komi=0.5, render_mode="rgb_array")
+        env.reset(seed=0)
     elif env_type == "go":
-        env = go_v5.raw_env(board_size = 9,komi = 6.5,render_mode='rgb_array')
-        env.reset(seed = 0)
+        env = go_v5.raw_env(board_size=9, komi=6.5, render_mode="rgb_array")
+        env.reset(seed=0)
     elif env_type == "chess":
         env = chess_v6.raw_env(render_mode="rgb_array")
-        env.reset(seed = 0)
+        env.reset(seed=0)
     elif env_type == "tictactoe":
-        env = tictactoe_v3.raw_env(render_mode='rgb_array')
-        env.reset(seed = 0)
+        env = tictactoe_v3.raw_env(render_mode="rgb_array")
+        env.reset(seed=0)
     elif env_type == "connect4":
-        env = connect_four_v3.raw_env(render_mode = "rgb_array")
-        env.reset(seed = 0)
+        env = connect_four_v3.raw_env(render_mode="rgb_array")
+        env.reset(seed=0)
     return env
