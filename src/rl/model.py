fbaad0714de5de9656475f82000570fb6ef82d44
Capiru <gabriel.silva.2@deliveryhero.com>
2025-04-12 10:29:36 +0200

    fix model for proper params per size

--- src/rl/model.py
+++ src/rl/model.py
@@ -43,12 +43,8 @@ class SimpleModel(nn.Module):
         else:
             x = torch.flatten(x)
         out = F.gelu(self.input_l(x))
-        res = out
         for i in range(self.n_layers):
             out = F.gelu(self.layers[f"lin_{i}"](out))
-            if i % 2 == 0 and i > 0:
-                out += res
-                res = out
         policy = F.softmax(self.policy_h(out), dim=-1)
         value = F.tanh(self.value_h(out))
         return policy, value
@@ -65,19 +61,21 @@ class SimpleConvnet(nn.Module):
         self.cfg = cfg
         a, b, c = cfg.obs_size[cfg.env_type]
         self.layers = nn.ModuleDict()
-        self.n_layers = n_layers
+        self.num_neurons = num_neurons
+        self.n_layers = n_layers * 2
         self.conv1 = nn.Conv2d(c, num_neurons, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(num_neurons, num_neurons * 2, kernel_size=3, padding=1)
-        self.conv3 = nn.Conv2d(
-            num_neurons * 2, num_neurons * 3, kernel_size=3, padding=1
-        )
-        self.conv4 = nn.Conv2d(
-            num_neurons * 3, num_neurons * 4, kernel_size=3, padding=1
-        )
-        self.conv5 = nn.Conv2d(
-            num_neurons * 4, num_neurons * 4, kernel_size=3, padding=1
-        )
-        self.mlp_input = nn.Linear(a * b * num_neurons * 4, num_neurons)
+        for i in range(self.n_layers):
+            self.layers[f"conv_{i}_1"] = nn.Conv2d(
+                self.num_neurons, self.num_neurons, kernel_size=3, padding=1
+            )
+            self.layers[f"bn_{i}_1"] = nn.BatchNorm2d(self.num_neurons)
+            self.layers[f"dout_{i}_1"] = nn.Dropout(p=cfg.dropout)
+            self.layers[f"conv_{i}_2"] = nn.Conv2d(
+                self.num_neurons, self.num_neurons, kernel_size=3, padding=1
+            )
+            self.layers[f"bn_{i}_2"] = nn.BatchNorm2d(self.num_neurons)
+            self.layers[f"dout_{i}_2"] = nn.Dropout(p=cfg.dropout)
+        self.mlp_input = nn.Linear(a * b * num_neurons, num_neurons)
         self.mlp1 = nn.Linear(num_neurons, num_neurons)
         self.mlp2 = nn.Linear(num_neurons, num_neurons)
 
@@ -95,10 +93,12 @@ class SimpleConvnet(nn.Module):
             x = x.permute(2, 0, 1)
 
         x = F.leaky_relu(self.conv1(x))
-        x = F.leaky_relu(self.conv2(x))
-        x = F.leaky_relu(self.conv3(x))
-        x = F.leaky_relu(self.conv4(x))
-        x = F.leaky_relu(self.conv5(x))
+        for i in range(self.n_layers):
+            x = F.leaky_relu(self.layers[f"bn_{i}_1"](self.layers[f"conv_{i}_1"](x)))
+            x = self.layers[f"dout_{i}_1"](x)
+            x = self.layers[f"bn_{i}_2"](self.layers[f"conv_{i}_2"](x))
+            x = F.leaky_relu(x)
+            x = self.layers[f"dout_{i}_2"](x)
         if len(x.shape) > 3:
             x = torch.flatten(x, 1)
         else:
@@ -123,7 +123,7 @@ class AlphaGoZeroResnet(nn.Module):
         self.cfg = cfg
         a, b, c = cfg.obs_size[cfg.env_type]
         self.layers = nn.ModuleDict()
-        self.n_layers = n_layers
+        self.n_layers = n_layers * 2
         self.num_neurons = num_neurons
 
         # Input Layer
